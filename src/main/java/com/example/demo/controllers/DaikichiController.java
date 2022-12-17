965a871434052ec410917f1b1a901ca648565ed6
katkola <katkola@gmail.com>
2022-12-16 21:28:05 -0500

    Update DaikichiController.java

--- src/main/java/com/example/demo/controllers/DaikichiController.java
+++ src/main/java/com/example/demo/controllers/DaikichiController.java
@@ -1,29 +1,13 @@
 package com.example.demo.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DaikichiController {
-	
-	@RequestMapping("/")
-	public String index() {
-		return "Hello World";
-	}
-	
-	@RequestMapping("/daikichi")
-	public String welcome() {
-		return "Welcome";
-	}
-	
-	@GetMapping("/daikichi/today")
-	public String lucky() {
-		return  "Today you will find luck in all your endeavors!";
-	}
-	@GetMapping("/daikichi/tomorrow")
-	public String tomorrow() {
-		return  "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
-	}
-
+    @RequestMapping("/")
+    public String index(@RequestParam(value="q") String searchQuery) {
+        return "You searched for: " + searchQuery;
+    }
 }
